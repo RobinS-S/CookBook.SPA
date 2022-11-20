abcaff395183c419e79adf98dbe2f86ad2457527
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2022-11-20 01:11:06 +0100

    [#1] [ADD] Basic authentication and PostgreSQL working

--- CookBook/Data/ApplicationDbContext.cs
+++ CookBook/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using CookBook.Models;
+using Duende.IdentityServer.EntityFramework.Entities;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -11,7 +13,28 @@ namespace CookBook.Data
         public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
             : base(options, operationalStoreOptions)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Set PGSQL default schema
+            builder.HasDefaultSchema(DbConstants.SchemaName);
+
+            // Apply EntityTypeConfigurations
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            // Modify IdentityDbContext to match PGSQL's naming conventions to avoid issues with querying
+            builder.Entity<ApplicationUser>().ToTable("asp_net_users", DbConstants.SchemaName);
+            builder.Entity<IdentityUserToken<string>>().ToTable("asp_net_user_tokens", DbConstants.SchemaName);
+            builder.Entity<IdentityUserLogin<string>>().ToTable("asp_net_user_logins", DbConstants.SchemaName);
+            builder.Entity<IdentityUserClaim<string>>().ToTable("asp_net_user_claims", DbConstants.SchemaName);
+            builder.Entity<IdentityRole>().ToTable("asp_net_roles", DbConstants.SchemaName);
+            builder.Entity<IdentityUserRole<string>>().ToTable("asp_net_user_roles", DbConstants.SchemaName);
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("asp_net_role_claims", DbConstants.SchemaName);
+            builder.Entity<PersistedGrant>().ToTable("identity_persisted_grants", DbConstants.SchemaName);
+            builder.Entity<DeviceFlowCodes>().ToTable("identity_device_flow_codes", DbConstants.SchemaName);
         }
     }
 }
---
+++ CookBook/Data/Migrations/20221117222553_Initial.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CookBook.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "cook_book");
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_roles",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_users",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    user_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_user_name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    normalized_email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    email_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: true),
+                    security_stamp = table.Column<string>(type: "text", nullable: true),
+                    concurrency_stamp = table.Column<string>(type: "text", nullable: true),
+                    phone_number = table.Column<string>(type: "text", nullable: true),
+                    phone_number_confirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    two_factor_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    lockout_end = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    lockout_enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    access_failed_count = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "identity_device_flow_codes",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    user_code = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    device_code = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    subject_id = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    session_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    client_id = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    creation_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expiration = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    data = table.Column<string>(type: "character varying(50000)", maxLength: 50000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_identity_device_flow_codes", x => x.user_code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "identity_persisted_grants",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    key = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    subject_id = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    session_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    client_id = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    creation_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expiration = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    consumed_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    data = table.Column<string>(type: "character varying(50000)", maxLength: 50000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_identity_persisted_grants", x => x.key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "keys",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    version = table.Column<int>(type: "integer", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    use = table.Column<string>(type: "text", nullable: true),
+                    algorithm = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    is_x509certificate = table.Column<bool>(type: "boolean", nullable: false),
+                    data_protected = table.Column<bool>(type: "boolean", nullable: false),
+                    data = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_keys", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_role_claims",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    role_id = table.Column<string>(type: "text", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_role_claims", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_asp_net_role_claims_asp_net_roles_role_id",
+                        column: x => x.role_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_user_claims",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    claim_type = table.Column<string>(type: "text", nullable: true),
+                    claim_value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_claims", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_claims_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_user_logins",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    login_provider = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    provider_key = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    provider_display_name = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_logins", x => new { x.login_provider, x.provider_key });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_logins_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_user_roles",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    role_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_roles", x => new { x.user_id, x.role_id });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_roles_asp_net_roles_role_id",
+                        column: x => x.role_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_roles_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asp_net_user_tokens",
+                schema: "cook_book",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    login_provider = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_asp_net_user_tokens", x => new { x.user_id, x.login_provider, x.name });
+                    table.ForeignKey(
+                        name: "fk_asp_net_user_tokens_asp_net_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "cook_book",
+                        principalTable: "asp_net_users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_role_claims_role_id",
+                schema: "cook_book",
+                table: "asp_net_role_claims",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "cook_book",
+                table: "asp_net_roles",
+                column: "normalized_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_claims_user_id",
+                schema: "cook_book",
+                table: "asp_net_user_claims",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_logins_user_id",
+                schema: "cook_book",
+                table: "asp_net_user_logins",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_user_roles_role_id",
+                schema: "cook_book",
+                table: "asp_net_user_roles",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "cook_book",
+                table: "asp_net_users",
+                column: "normalized_email");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "cook_book",
+                table: "asp_net_users",
+                column: "normalized_user_name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_device_flow_codes_device_code",
+                schema: "cook_book",
+                table: "identity_device_flow_codes",
+                column: "device_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_device_flow_codes_expiration",
+                schema: "cook_book",
+                table: "identity_device_flow_codes",
+                column: "expiration");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_persisted_grants_consumed_time",
+                schema: "cook_book",
+                table: "identity_persisted_grants",
+                column: "consumed_time");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_persisted_grants_expiration",
+                schema: "cook_book",
+                table: "identity_persisted_grants",
+                column: "expiration");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_persisted_grants_subject_id_client_id_type",
+                schema: "cook_book",
+                table: "identity_persisted_grants",
+                columns: new[] { "subject_id", "client_id", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_persisted_grants_subject_id_session_id_type",
+                schema: "cook_book",
+                table: "identity_persisted_grants",
+                columns: new[] { "subject_id", "session_id", "type" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_keys_use",
+                schema: "cook_book",
+                table: "keys",
+                column: "use");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "asp_net_role_claims",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_user_claims",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_user_logins",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_user_roles",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_user_tokens",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "identity_device_flow_codes",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "identity_persisted_grants",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "keys",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_roles",
+                schema: "cook_book");
+
+            migrationBuilder.DropTable(
+                name: "asp_net_users",
+                schema: "cook_book");
+        }
+    }
+}
